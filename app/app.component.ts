4986dd98383ed902bbacec13e4e874f47edb1225
Alexandra Holcombe, Cassie Musolf and Marc Larkin <alexandra+cassie+marc@>
2017-03-28 11:31:23 -0700

    Remove double edit click-listener

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,9 +10,38 @@ import { Component } from '@angular/core';
         <p>{{currentRecipe.ingredients}}</p>
         <h5>Instructions</h5>
         <p>{{currentRecipe.instructions}}</p>
+        <button type="button" (click)="editRecipe(currentRecipe)">Edit</button>
     </div>
 
     <div>
+
+    <!--EDIT FORM-->
+    <div *ngIf="selectedRecipe">
+        <form>
+        <input [(ngModel)]="selectedRecipe.recipeName"><br>
+        <input [(ngModel)]="selectedRecipe.ingredients"><br>
+        <input [(ngModel)]="selectedRecipe.instructions"><br>
+        <button (click)="hideEdit()">Hide</button>
+        </form>
+    </div>
+    <!--
+    <div *ngIf="selectedTask">
+         <h3>{{selectedTask.description}}</h3>
+         <p>Task Complete? {{selectedTask.done}}</p>
+         <hr>
+         <h3>Edit Task</h3>
+         <label>Enter Task Description:</label>
+         <input [(ngModel)]="selectedTask.description">
+         <label>Enter Task Priority (1-3):</label><br>
+         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+         <button (click)="finishedEditing()">Done</button>
+       </div>
+       -->
+
+
+    <h3>Add new recipe</h3>
         <form>
             <input #newRecipeName type="text" ng-model="recipeName" placeholder="Name of recipe">
             <br>
@@ -23,12 +52,26 @@ import { Component } from '@angular/core';
             <button (click)="submitForm(newRecipeName.value, newIngredients.value, newInstructions.value)">Add recipe</button>
         </form>
     </div>
-
   `,
 })
 export class AppComponent  {
 
     masterRecipeList: Recipe[] = [];
+
+    selectedRecipe = null;
+
+
+
+    hideEdit() {
+        this.selectedRecipe = null;
+    }
+
+    editRecipe(clickedRecipe) {
+        console.log("CLICKED: " + clickedRecipe);
+        debugger;
+        this.selectedRecipe = clickedRecipe;
+        console.log("SELECTED: " + selectedRecipe);
+    }
 
     addRecipe(newRecipe: Recipe) {
         this.masterRecipeList.push(newRecipe);
@@ -38,9 +81,7 @@ export class AppComponent  {
         console.log(newRecipeName + ", " + newIngredients + ", " + newInstructions);
         var newRecipe: Recipe = new Recipe (newRecipeName, newIngredients, newInstructions);
         this.masterRecipeList.push(newRecipe);
-        // addRecipe(newRecipe);
     }
-
 }
 
 export class Recipe {
