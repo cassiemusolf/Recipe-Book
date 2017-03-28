ac58ee58946e517a591e71f33b4e3ca9a73c2177
Alexandra Holcombe, Cassie Musolf and Marc Larkin <alexandra+cassie+marc@>
2017-03-28 13:28:35 -0700

    add filler content

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,67 +10,38 @@ import { Component } from '@angular/core';
         <p>{{currentRecipe.ingredients}}</p>
         <h5>Instructions</h5>
         <p>{{currentRecipe.instructions}}</p>
-        <button type="button" (click)="editRecipe(currentRecipe)">Edit</button>
+        <button (click)="editRecipe()">Edit</button>
     </div>
 
     <div>
-
-    <!--EDIT FORM-->
-    <div *ngIf="selectedRecipe">
-        <form>
-        <input [(ngModel)]="selectedRecipe.recipeName"><br>
-        <input [(ngModel)]="selectedRecipe.ingredients"><br>
-        <input [(ngModel)]="selectedRecipe.instructions"><br>
-        <button (click)="hideEdit()">Hide</button>
-        </form>
-    </div>
-    <!--
-    <div *ngIf="selectedTask">
-         <h3>{{selectedTask.description}}</h3>
-         <p>Task Complete? {{selectedTask.done}}</p>
-         <hr>
-         <h3>Edit Task</h3>
-         <label>Enter Task Description:</label>
-         <input [(ngModel)]="selectedTask.description">
-         <label>Enter Task Priority (1-3):</label><br>
-         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-         <button (click)="finishedEditing()">Done</button>
-       </div>
-       -->
-
-
-    <h3>Add new recipe</h3>
-        <form>
-            <input #newRecipeName type="text" ng-model="recipeName" placeholder="Name of recipe">
-            <br>
-            <textarea #newIngredients ng-model="ingredients" placeholder="Ingredients"></textarea>
-            <br>
-            <textarea #newInstructions ng-model="instructions" placeholder="Cooking instructions"></textarea>
-            <br>
-            <button (click)="submitForm(newRecipeName.value, newIngredients.value, newInstructions.value)">Add recipe</button>
-        </form>
+        <h3>Add new recipe</h3>
+            <form>
+                <input #newRecipeName type="text" ng-model="recipeName" placeholder="Name of recipe">
+                <br>
+                <textarea #newIngredients ng-model="ingredients" placeholder="Ingredients"></textarea>
+                <br>
+                <textarea #newInstructions ng-model="instructions" placeholder="Cooking instructions"></textarea>
+                <br>
+                <button (click)="submitForm(newRecipeName.value, newIngredients.value, newInstructions.value)">Add recipe</button>
+            </form>
     </div>
   `,
 })
 export class AppComponent  {
 
-    masterRecipeList: Recipe[] = [];
+    masterRecipeList: Recipe[] = [
+        new Recipe('SpagBol', 'Spag and Bol', 'Combine Spag with Bol'),
+        new Recipe('Grilled Cheese', 'Grill and Cheese', 'Use Grill to Cheese')
+    ];
 
     selectedRecipe = null;
-
-
 
     hideEdit() {
         this.selectedRecipe = null;
     }
 
-    editRecipe(clickedRecipe) {
-        console.log("CLICKED: " + clickedRecipe);
-        debugger;
-        this.selectedRecipe = clickedRecipe;
-        console.log("SELECTED: " + selectedRecipe);
+    editRecipe() {
+        alert("Button clicky")
     }
 
     addRecipe(newRecipe: Recipe) {
@@ -88,3 +59,32 @@ export class Recipe {
     constructor( public recipeName: string, public ingredients: string, public instructions: string){ }
 
 }
+
+
+
+//
+// <!--EDIT FORM-->
+// <!--    <div *ngIf="selectedRecipe">
+// <form> -->
+// <!-- <input [(ngModel)]="selectedRecipe.recipeName"><br>
+// <input [(ngModel)]="selectedRecipe.ingredients"><br>
+// <input [(ngModel)]="selectedRecipe.instructions"><br>
+// <button (click)="hideEdit()">Hide</button> -->
+// <!-- </form>
+// </div> -->
+// <!--
+// <div *ngIf="selectedTask">
+// <h3>{{selectedTask.description}}</h3>
+// <p>Task Complete? {{selectedTask.done}}</p>
+// <hr>
+// <h3>Edit Task</h3>
+// <label>Enter Task Description:</label>
+// <input [(ngModel)]="selectedTask.description">
+// <label>Enter Task Priority (1-3):</label><br>
+// <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+// <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+// <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+// <button (click)="finishedEditing()">Done</button>
+// </div>
+// -->
+//
